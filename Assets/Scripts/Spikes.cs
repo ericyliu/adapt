733cdcf413868bd4858ae03fa84ca8eede62a436
Eric Liu <eyliubit@gmail.com>
2019-06-16 16:44:37 -0400

    Added Spikes

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+  public int damage = 3;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Transform model = this.transform.Find("Model");
+    GameObject prefab = (GameObject.Find("Prefabs").GetComponent<Prefabs>() as Prefabs).spikes;
+    Instantiate(prefab, model);
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    Creature enemy = other.gameObject.GetComponent<Creature>();
+    if (enemy == null) return;
+    enemy.TakeDamage(this.damage);
+  }
+}
