715102d9c8a3ca44e8c732814b958668e45ffec0
Eric Liu <eyliubit@gmail.com>
2019-06-16 15:59:54 -0400

    split up player controller, add enemy

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+  public Transform model;
+  public float speed = 3f;
+  public float baseSpeed = 3f;
+  public float fury = 0f;
+  public float agility = 0f;
+  public float essence = 0f;
+  public float health = 100f;
+  public float maxHealth = 100f;
+  public float baseHealth = 100f;
+  // Start is called before the first frame update
+  void Start()
+  {
+    health = maxHealth;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    HandleStats();
+  }
+
+  void HandleStats()
+  {
+    speed = baseSpeed + (agility / 10f);
+    maxHealth = baseHealth + fury;
+    float size = 1f + (.01f * fury);
+    this.model.localScale = new Vector3(size, size, size);
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    Amoeba amoeba = other.gameObject.GetComponent<Amoeba>();
+    if (amoeba == null) return;
+    if (amoeba.type == AmoebaType.Agility) agility++;
+    else if (amoeba.type == AmoebaType.Essence) essence++;
+    else if (amoeba.type == AmoebaType.Fury)
+    {
+      health = Mathf.Min(health + 1, maxHealth + 1);
+      fury++;
+    }
+    GameObject.Destroy(other.gameObject);
+  }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,10 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-  public float speed = 3f;
-  public float baseSpeed = 3f;
-  public float fury = 0f;
-  public float agility = 0f;
-  public float essence = 0f;
-  public float health = 100f;
-  public float maxHealth = 100f;
-  public float baseHealth = 100f;
   public Camera playerCamera;
-  public Terrain terrain;
-  public Transform model;
   public Text score;
+  public Transform model;
+  public Creature creature;
 
   public ClientControlState clientControlState;
 
@@ -32,26 +24,11 @@ public class PlayerController : MonoBehaviour
     this.transform.position = newPosition;
   }
 
-  // Use this for initialization
-  void Start()
-  {
-    health = maxHealth;
-  }
-
   // Update is called once per frame
   void Update()
   {
-    HandleStats();
     HandleMovement();
     HandleUI();
-  }
-
-  void HandleStats()
-  {
-    speed = baseSpeed + (agility / 10f);
-    maxHealth = baseHealth + fury;
-    float size = 1f + (.01f * fury);
-    model.localScale = new Vector3(size, size, size);
   }
 
   void HandleMovement()
@@ -66,20 +43,6 @@ public class PlayerController : MonoBehaviour
 
   void HandleUI()
   {
-    score.text = "Health: " + health + "/" + maxHealth + "\nAgility: " + agility + "\nEssence: " + essence + "\nFury: " + fury;
-  }
-
-  void OnTriggerEnter(Collider other)
-  {
-    Amoeba amoeba = other.gameObject.GetComponent<Amoeba>();
-    if (amoeba == null) return;
-    if (amoeba.type == AmoebaType.Agility) agility++;
-    else if (amoeba.type == AmoebaType.Essence) essence++;
-    else if (amoeba.type == AmoebaType.Fury)
-    {
-      health = Mathf.Min(health + 1, maxHealth + 1);
-      fury++;
-    }
-    GameObject.Destroy(other.gameObject);
+    score.text = "Health: " + creature.health + "/" + creature.maxHealth + "\nAgility: " + creature.agility + "\nEssence: " + creature.essence + "\nFury: " + creature.fury;
   }
 }
